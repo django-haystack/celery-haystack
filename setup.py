8a818df563b9da265a4e456dde6be9147957acce
Jannis Leidel <jannis@leidel.info>
2011-07-29 23:19:30 +0200

    Bumped version and added trove classifier for supported Python version.

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Utilities',
     ],
     setup_requires=[
