8cb0d1a9aff1c86837d7c14c74b95127e47b7315
Jannis Leidel <jannis@leidel.info>
2011-12-03 23:40:31 +0100

    Use unittest-xml-reporting for proper XML reporting.

--- celery_haystack/tests/settings/v2.py
+++ celery_haystack/tests/settings/v2.py
@@ -1,4 +1,4 @@
-from os import path
+from os import path, environ
 
 TEST_ROOT = path.abspath(path.join(path.dirname(__file__), '..'))
 
@@ -28,3 +28,9 @@ CELERY_ALWAYS_EAGER = True
 CELERY_IGNORE_RESULT = True
 CELERYD_LOG_LEVEL = "DEBUG"
 CELERY_DEFAULT_QUEUE = "celery-haystack"
+
+if environ.get('HUDSON_URL', None):
+    TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+    TEST_OUTPUT_VERBOSE = True
+    TEST_OUTPUT_DESCRIPTIONS = True
+    TEST_OUTPUT_DIR = 'xmlrunner'
