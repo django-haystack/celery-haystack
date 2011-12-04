52ccdec5813ae85931c0b07bc38bba9f10f87198
Jannis Leidel <jannis@leidel.info>
2011-12-04 01:24:57 +0100

    Use nose as the test runner to get the XML easily.

--- celery_haystack/tests/settings/v1.py
+++ celery_haystack/tests/settings/v1.py
@@ -1,8 +1,9 @@
 from os import path, environ, pardir
 
-TEST_ROOT = path.abspath(path.join(path.dirname(__file__), pardir, pardir))
+TEST_ROOT = path.abspath(path.join(path.dirname(__file__), pardir))
 
 INSTALLED_APPS = [
+    'django_nose',
     'haystack',
     'djcelery',
     'celery_haystack',
@@ -26,8 +27,4 @@ CELERY_IGNORE_RESULT = True
 CELERYD_LOG_LEVEL = "DEBUG"
 CELERY_DEFAULT_QUEUE = "celery-haystack"
 
-if environ.get('HUDSON_URL', None):
-    TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
-    TEST_OUTPUT_VERBOSE = True
-    TEST_OUTPUT_DESCRIPTIONS = True
-    TEST_OUTPUT_DIR = 'xmlrunner'
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
