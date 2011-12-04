0394e7a19dc1d1bbbe241969325e95a149ff9dd7
Jannis Leidel <jannis@leidel.info>
2011-12-04 13:33:21 +0100

    Added a base settings module and use more PyFlakes and pep8 tasks, too.

--- celery_haystack/tests/settings/v2.py
+++ celery_haystack/tests/settings/v2.py
@@ -1,21 +1,4 @@
-from os import path, environ, pardir
-
-TEST_ROOT = path.abspath(path.join(path.dirname(__file__), pardir))
-
-INSTALLED_APPS = [
-    'django_jenkins',
-    'haystack',
-    'djcelery',
-    'celery_haystack',
-    'celery_haystack.tests',
-]
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    }
-}
+from .base import *
 
 HAYSTACK_CONNECTIONS = {
     'default': {
@@ -23,9 +6,3 @@ HAYSTACK_CONNECTIONS = {
         'PATH': path.join(TEST_ROOT, 'whoosh_index'),
     }
 }
-
-BROKER_TRANSPORT = "memory"
-CELERY_ALWAYS_EAGER = True
-CELERY_IGNORE_RESULT = True
-CELERYD_LOG_LEVEL = "DEBUG"
-CELERY_DEFAULT_QUEUE = "celery-haystack"
