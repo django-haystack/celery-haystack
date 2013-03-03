0b0ceb5d86a3966752c8674ee96dedb949011384
Jannis Leidel <jannis@leidel.info>
2013-03-03 13:10:45 +0100

    Added some code comments for the settings.

--- celery_haystack/conf.py
+++ celery_haystack/conf.py
@@ -4,17 +4,28 @@ from appconf import AppConf
 
 
 class CeleryHaystack(AppConf):
+    #: The default alias to
     DEFAULT_ALIAS = None
+    #: The delay (in seconds) after which a failed index is retried
     RETRY_DELAY = 5 * 60
+    #: The number of retries that are done
     MAX_RETRIES = 1
+    #: The default Celery task class
     DEFAULT_TASK = 'celery_haystack.tasks.CeleryHaystackSignalHandler'
+    #: Whether the task should be handled transaction safe
     TRANSACTION_SAFE = True
 
+    #: The batch size used by the CeleryHaystackUpdateIndex task
     COMMAND_BATCH_SIZE = None
+    #: The max age of items used by the CeleryHaystackUpdateIndex task
     COMMAND_AGE = None
+    #: Wehther to remove items from the index that aren't in the DB anymore
     COMMAND_REMOVE = False
+    #: The number of multiprocessing workers used by the CeleryHaystackUpdateIndex task
     COMMAND_WORKERS = 0
+    #: The names of apps to run update_index for
     COMMAND_APPS = []
+    #: The verbosity level of the update_index call
     COMMAND_VERBOSITY = 1
 
     def configure_default_alias(self, value):
