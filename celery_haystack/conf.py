1c7e4e2a66d367a20831eb56945d5293a6f99f96
Jannis Leidel <jannis@leidel.info>
2013-03-09 15:22:54 +0100

    Raise an exception if using Haystack 2.X and not having defined a HAYSTACK_SIGNAL_PROCESSOR setting.

--- celery_haystack/conf.py
+++ celery_haystack/conf.py
@@ -1,5 +1,6 @@
 from django.conf import settings  # noqa
-from haystack import constants
+from django.core.exceptions import ImproperlyConfigured
+from haystack import constants, __version__ as haystack_version
 from appconf import AppConf
 
 
@@ -39,3 +40,12 @@ class CeleryHaystack(AppConf):
                 value = int(value)
             data[name] = value
         return data
+
+if (haystack_version[0] >= 2 and
+    getattr(settings, 'HAYSTACK_SIGNAL_PROCESSOR', None) !=
+        'celery_haystack.signals.CelerySignalProcessor'):
+
+    raise ImproperlyConfigured("When using celery-haystack with Haystack 2.X "
+                               "the HAYSTACK_SIGNAL_PROCESSOR setting must be "
+                               "set to 'celery_haystack.signals."
+                               "CelerySignalProcessor'.")
