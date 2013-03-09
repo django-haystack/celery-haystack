0180b996300d919befb487ce18e1ffdba44b7e03
Jannis Leidel <jannis@leidel.info>
2013-03-09 15:21:56 +0100

    Simplified the signal processor to be closer to the queued_search implementation.

--- celery_haystack/signals.py
+++ celery_haystack/signals.py
@@ -1,52 +1,47 @@
-from haystack.signals import RealtimeSignalProcessor
+from django.db.models import signals
+
+from haystack.signals import BaseSignalProcessor
 from haystack.utils import get_identifier
 from haystack.exceptions import NotHandled
 
-from celery_haystack.utils import get_update_task
-from celery_haystack.indexes import CelerySearchIndex
+from .utils import get_update_task
+from .indexes import CelerySearchIndex
 
 
-class CelerySignalProcessor(RealtimeSignalProcessor):
-    def __init__(self, *args, **kwargs):
-        super(CelerySignalProcessor, self).__init__(*args, **kwargs)
+class CelerySignalProcessor(BaseSignalProcessor):
+
+    def setup(self):
         self.task_cls = get_update_task()
+        signals.post_save.connect(self.enqueue_save)
+        signals.post_delete.connect(self.enqueue_delete)
 
-    def handle_save(self, sender, instance, **kwargs):
-        return self.handle(sender, instance, action='update', **kwargs)
+    def teardown(self):
+        signals.post_save.disconnect(self.enqueue_save)
+        signals.post_delete.disconnect(self.enqueue_delete)
 
-    def handle_delete(self, sender, instance, **kwargs):
-        return self.handle(sender, instance, action='delete', **kwargs)
+    def enqueue_save(self, sender, instance, **kwargs):
+        return self.enqueue('update', instance, sender, **kwargs)
 
-    def handle(self, sender, instance, action='update', **kwargs):
+    def enqueue_delete(self, sender, instance, **kwargs):
+        return self.enqueue('delete', instance, sender, **kwargs)
+
+    def enqueue(self, action, instance, sender, **kwargs):
         """
         Given an individual model instance, determine if a backend
         handles the model, check if the index is Celery-enabled and
         enqueue task.
         """
-
         using_backends = self.connection_router.for_write(instance=instance)
 
         for using in using_backends:
             try:
-                index = (self.connections[using].get_unified_index()
-                         .get_index(sender))
+                connection = self.connections[using]
+                index = connection.get_unified_index().get_index(sender)
             except NotHandled:
                 continue  # Check next backend
 
             if isinstance(index, CelerySearchIndex):
                 if action == 'update' and not index.should_update(instance):
                     continue
-                self.enqueue(action, instance)
+                self.task_cls.delay(action, get_identifier(instance))
                 return  # Only enqueue instance once
-
-    def enqueue(self, action, instance):
-        """
-        Shoves a message about how to update the index into the queue.
-
-        This is a standardized string, resembling something like::
-
-            ``notes.note.23``
-            # ...or...
-            ``weblog.entry.8``
-        """
-        return self.task_cls.delay(action, get_identifier(instance))
