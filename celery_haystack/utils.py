19ac7564c3a7cb5de13e07d895336a586c81582c
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2015-12-11 13:16:32 -0200

    Fix missing importlib.

--- celery_haystack/utils.py
+++ celery_haystack/utils.py
@@ -1,5 +1,9 @@
+try:
+    from importlib import import_module
+except ImportError:
+    from django.utils.importlib import import_module
+
 from django.core.exceptions import ImproperlyConfigured
-from django.utils.importlib import import_module
 from django.db import connection
 
 from haystack.utils import get_identifier
