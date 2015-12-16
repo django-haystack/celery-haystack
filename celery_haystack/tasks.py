2e7a4c18e2bc290a100138949946e863b73dea61
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2015-12-16 09:36:09 -0200

    Fix import.

--- celery_haystack/tasks.py
+++ celery_haystack/tasks.py
@@ -1,6 +1,11 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.core.management import call_command
-from django.db.models.loading import get_model
+
+try:
+    from django.apps import apps
+    get_model = apps.get_model
+except ImportError:
+    from django.db.models.loading import get_model
 
 from .conf import settings
 
