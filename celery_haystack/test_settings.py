b03eb84f1892a7cf40392a8b86f8bf782b57958c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-03-21 11:52:52 +0100

    Remove Django-Celery dependency

--- celery_haystack/test_settings.py
+++ celery_haystack/test_settings.py
@@ -5,14 +5,12 @@ from celery import Celery
 app = Celery('celery_haystack')
 app.config_from_object('django.conf:settings')
 
-
 DEBUG = True
 
 TEST_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), 'tests'))
 
 INSTALLED_APPS = [
     'haystack',
-    'djcelery',
     'celery_haystack',
     'celery_haystack.tests',
 ]
