2fd816d9f073895cad9a8c0fa56951cbd6c743a7
John Obelenus <jobelenus@gmail.com>
2016-07-29 11:17:32 -0400

    next attempt at getting settings working with a proper app config to make sure this file runs

---
+++ celery_haystack/apps.py
@@ -0,0 +1,6 @@
+from .conf import  # this populates our settings
+from django.apps import apps as django_apps
+
+
+class CeleryHaystackAppConfig(django_apps.AppConfig):
+    name = "celery_haystack"
