57b542eaf58d8be46b08459480b4de71bface28c
John Obelenus <jobelenus@gmail.com>
2016-07-29 10:56:45 -0400

    Revert "i think this is what i need to do for task subclassing"
    This reverts commit 48a675b226bd425561bb706fb222f3743b30563a.

--- celery_haystack/__init__.py
+++ celery_haystack/__init__.py
@@ -1,9 +1,5 @@
 __version__ = '0.10'
 
 
-# this is not a django AppConfig object and therefore needs to be imported here so other tasks can subclass CeleryHaystackSignalHandler
-from .conf import CeleryHaystack
-
-
 def version_hook(config):
     config['metadata']['version'] = __version__
