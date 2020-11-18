4048e5b189543c32cf3764081c929cf094300d7d
Dominik George <nik@naturalnet.de>
2020-11-18 22:15:18 +0100

    Add setting CELERY_HAYSTACK_IGNORE_RESULT
    If True, results of the signal handler task will not be written to any
    result store to not produce large amounts of irrelevant results.
    
    Errors will still be stored.

--- celery_haystack/conf.py
+++ celery_haystack/conf.py
@@ -20,6 +20,8 @@ class CeleryHaystack(AppConf):
     QUEUE = None
     #: Whether the task should be handled transaction safe
     TRANSACTION_SAFE = True
+    #: Whether the task results should be ignored
+    IGNORE_RESULT = False
 
     #: The batch size used by the CeleryHaystackUpdateIndex task
     COMMAND_BATCH_SIZE = None
