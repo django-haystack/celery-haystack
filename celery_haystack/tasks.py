4a41600d57ce6580b7c5a30d8f5e66da15ef99bf
Restyled.io <commits@restyled.io>
2020-11-29 13:10:25 +0000

    Restyled by black

--- celery_haystack/tasks.py
+++ celery_haystack/tasks.py
@@ -24,30 +24,30 @@ class CeleryHaystackSignalHandler(Task):
 
         Converts 'notes.note.23' into ('notes.note', 23).
         """
-        bits = identifier.split('.')
+        bits = identifier.split(".")
 
         if len(bits) < 2:
-            logger.error("Unable to parse object "
-                         "identifer '%s'. Moving on..." % identifier)
+            logger.error(
+                "Unable to parse object " "identifer '%s'. Moving on..." % identifier
+            )
             return (None, None)
 
         pk = bits[-1]
         # In case Django ever handles full paths...
-        object_path = '.'.join(bits[:-1])
+        object_path = ".".join(bits[:-1])
         return (object_path, pk)
 
     def get_model_class(self, object_path, **kwargs):
         """
         Fetch the model's class in a standarized way.
         """
-        bits = object_path.split('.')
-        app_name = '.'.join(bits[:-1])
+        bits = object_path.split(".")
+        app_name = ".".join(bits[:-1])
         classname = bits[-1]
         model_class = apps.get_model(app_name, classname)
 
         if model_class is None:
-            raise ImproperlyConfigured("Could not load model '%s'." %
-                                       object_path)
+            raise ImproperlyConfigured("Could not load model '%s'." % object_path)
         return model_class
 
     def get_instance(self, model_class, pk, **kwargs):
@@ -58,9 +58,14 @@ class CeleryHaystackSignalHandler(Task):
         try:
             instance = model_class._default_manager.get(pk=pk)
         except model_class.DoesNotExist:
-            logger.error("Couldn't load %s.%s.%s. Somehow it went missing?" %
-                         (model_class._meta.app_label.lower(),
-                          model_class._meta.object_name.lower(), pk))
+            logger.error(
+                "Couldn't load %s.%s.%s. Somehow it went missing?"
+                % (
+                    model_class._meta.app_label.lower(),
+                    model_class._meta.object_name.lower(),
+                    pk,
+                )
+            )
         except model_class.MultipleObjectsReturned:
             logger.error("More than one object with pk %s. Oops?" % pk)
         return instance
@@ -70,14 +75,14 @@ class CeleryHaystackSignalHandler(Task):
         Fetch the model's registered ``SearchIndex`` in a standarized way.
         """
         try:
-            using_backends = connection_router.for_write(
-                **{'models': [model_class]})
+            using_backends = connection_router.for_write(**{"models": [model_class]})
             for using in using_backends:
                 index_holder = connections[using].get_unified_index()
                 yield index_holder.get_index(model_class), using
         except IndexNotFoundException:
-            raise ImproperlyConfigured("Couldn't find a SearchIndex for %s." %
-                                       model_class)
+            raise ImproperlyConfigured(
+                "Couldn't find a SearchIndex for %s." % model_class
+            )
 
     def run(self, action, identifier, **kwargs):
         """
@@ -94,10 +99,11 @@ class CeleryHaystackSignalHandler(Task):
         # Then get the model class for the object path
         model_class = self.get_model_class(object_path, **kwargs)
         for current_index, using in self.get_indexes(model_class, **kwargs):
-            current_index_name = ".".join([current_index.__class__.__module__,
-                                           current_index.__class__.__name__])
+            current_index_name = ".".join(
+                [current_index.__class__.__module__, current_index.__class__.__name__]
+            )
 
-            if action == 'delete':
+            if action == "delete":
                 # If the object is gone, we'll use just the identifier
                 # against the index.
                 try:
@@ -106,15 +112,16 @@ class CeleryHaystackSignalHandler(Task):
                     logger.exception(exc)
                     self.retry(exc=exc)
                 else:
-                    msg = ("Deleted '%s' (with %s)" %
-                           (identifier, current_index_name))
+                    msg = "Deleted '%s' (with %s)" % (identifier, current_index_name)
                     logger.debug(msg)
-            elif action == 'update':
+            elif action == "update":
                 # and the instance of the model class with the pk
                 instance = self.get_instance(model_class, pk, **kwargs)
                 if instance is None:
-                    logger.debug("Failed updating '%s' (with %s)" %
-                                 (identifier, current_index_name))
+                    logger.debug(
+                        "Failed updating '%s' (with %s)"
+                        % (identifier, current_index_name)
+                    )
                     raise ValueError("Couldn't load object '%s'" % identifier)
 
                 # Call the appropriate handler of the current index and
@@ -125,8 +132,7 @@ class CeleryHaystackSignalHandler(Task):
                     logger.exception(exc)
                     self.retry(exc=exc)
                 else:
-                    msg = ("Updated '%s' (with %s)" %
-                           (identifier, current_index_name))
+                    msg = "Updated '%s' (with %s)" % (identifier, current_index_name)
                     logger.debug(msg)
             else:
                 logger.error("Unrecognized action '%s'. Moving on..." % action)
@@ -141,17 +147,17 @@ class CeleryHaystackUpdateIndex(Task):
 
     def run(self, apps=None, **kwargs):
         defaults = {
-            'batchsize': settings.CELERY_HAYSTACK_COMMAND_BATCH_SIZE,
-            'age': settings.CELERY_HAYSTACK_COMMAND_AGE,
-            'remove': settings.CELERY_HAYSTACK_COMMAND_REMOVE,
-            'using': [settings.CELERY_HAYSTACK_DEFAULT_ALIAS],
-            'workers': settings.CELERY_HAYSTACK_COMMAND_WORKERS,
-            'verbosity': settings.CELERY_HAYSTACK_COMMAND_VERBOSITY,
+            "batchsize": settings.CELERY_HAYSTACK_COMMAND_BATCH_SIZE,
+            "age": settings.CELERY_HAYSTACK_COMMAND_AGE,
+            "remove": settings.CELERY_HAYSTACK_COMMAND_REMOVE,
+            "using": [settings.CELERY_HAYSTACK_DEFAULT_ALIAS],
+            "workers": settings.CELERY_HAYSTACK_COMMAND_WORKERS,
+            "verbosity": settings.CELERY_HAYSTACK_COMMAND_VERBOSITY,
         }
         defaults.update(kwargs)
         if apps is None:
             apps = settings.CELERY_HAYSTACK_COMMAND_APPS
         # Run the update_index management command
         logger.info("Starting update index")
-        call_command('update_index', *apps, **defaults)
+        call_command("update_index", *apps, **defaults)
         logger.info("Finishing update index")
